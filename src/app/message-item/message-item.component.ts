10a0fe27f0f33e8e994688e0c5c7d094a42aa9e6
ubajack <jacques.bach@ubalia.fr>
2023-04-05 22:13:41 +0200

    Use a component to display messages

---
+++ src/app/message-item/message-item.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Message } from '../message';
+
+@Component({
+  selector: 'app-message-item',
+  templateUrl: './message-item.component.html',
+  styleUrls: ['./message-item.component.css']
+})
+export class MessageItemComponent {
+
+  @Input() message!: Message;
+
+}
